6cc1767b3aafedeedd9d5e0b3aeab1a546fc91fc
Fabio Nukui <fabio.nukui@gmail.com>
2021-11-15 22:09:26 -0300

    fix create_demux

--- terra_sdk/util/base.py
+++ terra_sdk/util/base.py
@@ -10,19 +10,25 @@ from .json import JSONSerializable, dict_to_data
 class BaseTerraData(JSONSerializable, Message):
 
     type: str
+    type_url: str
 
     def to_data(self) -> dict:
-        return {"type": self.type_url, "value": dict_to_data(self.__dict__)}
+        return {"@type": self.type_url, "value": dict_to_data(self.__dict__)}
+
+    @staticmethod
+    @abstractmethod
+    def from_data(data: dict) -> "BaseTerraData":
+        pass
 
     @abstractmethod
     def to_proto(self):
         pass
 
 
-def create_demux(inputs: List) -> Callable[[Dict[str, Any]], Any]:
+def create_demux(inputs: List[BaseTerraData]) -> Callable[[Dict[str, Any]], Any]:
     table = {i.type_url: i.from_data for i in inputs}
 
     def from_data(data: dict):
-        return table[data["@type"]](data)
+        return table[data["@type"]](data["value"])
 
     return from_data
